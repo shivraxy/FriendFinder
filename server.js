c311cabca672f6a22d5d9f2d8d5d02301a4ac4ce
shivraxy <31151144+shivraxy@users.noreply.github.com>
2018-02-18 00:02:41 -0800

    new

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let express = require("express");
-let port = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 let bodyParser = require("body-parser");
 
 let router = express.Router();
@@ -7,8 +7,8 @@ let htmlrouter = require("./app/routing/htmlRoutes.js");
 // let apirouter = require("./app/routing/apiRoutes.js")
 
 app = express();
-app.listen(port, function() {
-    console.log('Server started at port ' + port);
+app.listen(PORT, function() {
+    console.log('Server started at port ' + PORT);
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
